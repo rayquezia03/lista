eb705c6836544f4ffa72b4109c59a32909cbbdf8
rayquezia03 <rayquezia03@gmail.com>
2022-09-26 16:00:22 -0300

    final

--- js/L1Q7.js
+++ js/L1Q7.js
@@ -1,9 +1,3 @@
-//const nome = prompt('Digite o seu nome')
-//const idade = Number.parseInt(prompt('Digite a sua idade'))
-//const salario = Number.parseFloat(prompt('Digite o salario do usuário'))
-//const sexo = prompt('Digite o seu sexo')
-//const estado_civil = prompt('Digite o seu estado civil: s/c/v/d')
- 
 function validar_idade(idade){
     var idade = Number.parseInt(document.getElementById("idade").value);
  
@@ -48,36 +42,5 @@ function validar_estado(estado){
     }
 }
  
-/*
-if (nome.length <= 3) {
-    alert("Nome inválido")
-} else {
-    alert("Nome válido")
-}
- 
-if (idade > 0 && idade < 150) {
-    alert("Idade Válida")
-}else{
-    alert("Idade Inválida")
-}
- 
-if (salario != 0){
-    alert("Salário Válido")
-}else{
-    alert("Salário Iválido")
-}
- 
-if (sexo == 'f' || sexo == 'm'){
-    alert('sexo Válido')
-}else{
-    alert('sexo Inválido')
-}
- 
-if (estado_civil == 's' || estado_civil == 'c' || estado_civil == 'v' || estado_civil == 'd'){
-    alert('Estado Civil Válido')
-}else{
-    alert('Estado Civil Inválido')
-}
- 
-*/
+
  
